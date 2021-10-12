42335e9095e89c49d2f8454997e18786bd9e92d8
dmcin003 <dmcin003@fiu.edu>
2021-10-12 11:59:52 -0400

    Completed promiseConstructor.js exercises

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,33 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise((resolve, reject) =>{
+    fs.readFile(filePath, (err, fileData) =>{
+      if (err) {
+        reject(err);
+
+      } else {
+        var lines = fileData.toString().split('\n');
+        resolve(lines[0]);
+
+      }
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return new Promise((resolve, reject) =>{
+    request.get(url, null, (err, data) =>{
+      if (err) {
+        reject(err);
+      } else {
+
+        resolve(data.statusCode);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
